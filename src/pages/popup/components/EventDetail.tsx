b69d4542f1c5c080de77580860912805db6d5171
mins <davidwin3@gmail.com>
2025-02-15 10:44:01 +0900

    fix. Event detail info - fixed event item detail title.

--- src/pages/popup/components/EventDetail.tsx
+++ src/pages/popup/components/EventDetail.tsx
@@ -3,9 +3,12 @@ import { KinesisRequestData } from "@shared/types/request";
 export default function EventDetail({ event }: { event: KinesisRequestData }) {
   const { records } = event;
   const itemData = records?.[0] ?? {};
+  const { page_name, event_name } = itemData;
   return (
     <div className="w-1/2 border p-4 overflow-y-auto max-h-[460px]">
-      <h2 className="text-xl font-semibold mb-4">$identify</h2>
+      <h2 className="text-xl font-semibold mb-4">
+        {page_name} - {event_name}
+      </h2>
       <div className="space-y-4">
         {Object.entries(itemData).map(([key, value]) => (
           <div key={key} className="flex justify-between">
