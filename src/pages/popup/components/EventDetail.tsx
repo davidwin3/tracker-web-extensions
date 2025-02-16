f86bd837ad995e5ba30f9fdd8d57b62eefc3c284
mins <davidwin3@gmail.com>
2025-02-17 00:46:24 +0900

    feat. Add Filter keys only switch(in Event Detail) - Add dependency shadcn ui(canary). - Add Filter key only switch button.

--- src/pages/popup/components/EventDetail.tsx
+++ src/pages/popup/components/EventDetail.tsx
@@ -1,7 +1,8 @@
 import { KinesisRequestData } from "@shared/types/request";
 import { useEffect, useState } from "react";
-
+import { Switch } from "@src/components/ui/switch";
 export default function EventDetail({ event }: { event: KinesisRequestData }) {
+  const [enabledFilterKeysOnly, setEnabledFilterKeysOnly] = useState(false);
   const [filterEventDetailKeys, setFilterEventDetailKeys] = useState("");
 
   useEffect(() => {
@@ -10,26 +11,37 @@ export default function EventDetail({ event }: { event: KinesisRequestData }) {
     });
   }, []);
 
+  const handleFilterKeysOnly = () => {
+    setEnabledFilterKeysOnly(!enabledFilterKeysOnly);
+    chrome.storage.local.set({ enabledFilterKeysOnly });
+  };
+
   const { records } = event;
   const itemData = records?.[0] ?? {};
   const { page_name, event_name } = itemData;
   return (
     <div className="w-1/2 flex flex-col gap-2">
-      <span className="text-lg font-semibold">Event Detail</span>
-      <div className="w-full border p-4 overflow-y-auto max-h-[400px]">
+      <div className="flex justify-between items-center">
+        <span className="text-lg font-semibold">Event Detail</span>
+        <div className="flex items-center gap-2">
+          <Switch
+            checked={enabledFilterKeysOnly}
+            onCheckedChange={handleFilterKeysOnly}
+          />
+          <span className="text-sm text-gray-500">Filter keys only</span>
+        </div>
+      </div>
+      <div className="w-full border p-4 overflow-y-auto max-h-[425px]">
         <div className="flex justify-between">
           <h2 className="text-xl font-semibold mb-4">
             {page_name} - {event_name}
           </h2>
         </div>
-        <div className="flex justify-between">
-          <button type="button" className="text-sm text-gray-500">
-            Filter keys only
-          </button>
-        </div>
         <div className="space-y-4">
           {Object.entries(itemData)
-            .filter(([key]) => filterEventDetailKeys.includes(key))
+            .filter(([key]) =>
+              enabledFilterKeysOnly ? filterEventDetailKeys.includes(key) : true
+            )
             .map(([key, value]) => (
               <div key={key} className="flex justify-between">
                 <span className="text-gray-600">{key}</span>
